fef278e611bca956b0073a34dbcc279a4197088a
PacoZG <64326252+PacoZG@users.noreply.github.com>
2020-11-26 11:00:55 +0200

    6.3-6.8 Exercises

---
+++ Part 6/6.3-6.8/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
+
+const App = () => {
+  
+  return (
+    <div>
+      <h2>{'Anecdotes'}</h2>
+      <AnecdoteForm />
+      <AnecdoteList />
+      
+    </div>
+  )
+}
+
+export default App
---
+++ Part 6/6.3-6.8/src/components/AnecdoteList.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { voteFor } from '../reducers/anecdoteReducer'
+
+const AnecdoteList = () => {
+  const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  const vote = (id) => {
+    dispatch(voteFor(id))
+  }
+
+  return (
+    <div>
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            {`has ${anecdote.votes} votes`}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+export default AnecdoteList
